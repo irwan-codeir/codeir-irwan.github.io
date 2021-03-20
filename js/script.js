84745d6651ed58e019618e36e4581fbccff3467b
irwan.codeir@gmail.com <irwan-codeir>
2021-03-20 16:44:49 +0700

    update portfolio

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const navLink = document.querySelector('.nav-link');
+const button = document.querySelector('.navbar-toggler');
+const navCollapse = document.querySelector('.navbar-collapse');
+
+navLink.addEventListener('click', function(){
+	// navCollapse.style.display = "none";
+});
