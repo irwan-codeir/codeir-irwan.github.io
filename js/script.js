1bb997815b9649a5a9702d80733c7973b9a5128c
irwanesia <irwan.codeir@gmail.com>
2022-03-28 18:10:20 +0700

    update cv tailwindcss

--- js/script.js
+++
@@ -1,7 +0,0 @@
-const navLink = document.querySelector('.nav-link');
-const button = document.querySelector('.navbar-toggler');
-const navCollapse = document.querySelector('.navbar-collapse');
-
-navLink.addEventListener('click', function(){
-	// navCollapse.style.display = "none";
-});
